dc819889a4e4f7cad485284241bbec19e31fd263
topoptuser <topoptuser@gmail.com>
2018-01-09 21:25:15 -0200

    commit

--- hp-server-login.py
+++ hp-server-login.py
@@ -13,11 +13,3 @@ r = s.request(
    'http://10.10.180.1/web/device/login',
    data = hp_form,
 );
-
-r2 = s.request(
-   'GET',
-   'http://10.10.180.1/vld.bmp'
-);
-
-with open('vld.bmp', 'wb') as f:
-   f.write(r2.content);
