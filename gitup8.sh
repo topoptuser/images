a6cd5727c45ac41fb3ca39af6eb49167047cdf5c
topoptuser <topoptuser@gmail.com>
2018-01-09 21:30:30 -0200

    up8

---
+++ gitup8.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+git config --local user.name topoptuser
+git config --local user.email topoptuser@gmail.com
+git add .
+git commit -m up8
+git push origin master
--- hp-server-images.py
+++ hp-server-images.py
@@ -1,18 +1,18 @@
 import sys, time, requests
 
-s = requests.session()
-
 # defines the quantity of requests
 if len(sys.argv)>1:
    requests_quantity = int(sys.argv[1]);
-else;
+else:
    requests_quantity = 5;
 
 # get the images and saves it
 for i in range(requests_quantity):
    image_name =time.strftime("%Y%m%d%H%M%S", time.gmtime())+'.bmp';
+   s = requests.session()
    r2 = s.request(
       'GET',
       'http://10.10.180.1/vld.bmp' );
    with open( image_name, 'wb' ) as f:
       f.write(r2.content);
+   time.sleep(1);
